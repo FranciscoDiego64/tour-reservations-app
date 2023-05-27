871884da54fcf7d426e10f31d5ab9d542e972197
FranciscoDiego64 <112688191+FranciscoDiego64@users.noreply.github.com>
2023-05-27 13:00:47 +0200

    display user data

--- src/components/app.js
+++ src/components/app.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { auth } from '../firebase';
 import Login from './Login';
 import Profile from './Profile';
 import Reservation from './Reservation';
 import Calendar from './Calendar';
 
 const App = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      setUser(user);
+    });
+
+    // Cleanup subscription on unmount
+    return () => unsubscribe();
+  }, []);
+
+  if (!user) {
+    return <Login />;
+  }
+
   return (
     <div>
-      <Login />
-      <Profile />
-      <Reservation />
-      <Calendar />
+      <Profile user={user} />
+      <Reservation user={user} />
+      <Calendar user={user} />
     </div>
   );
 };
