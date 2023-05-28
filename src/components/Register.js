04cf021e5808b908806afc3cdec98359824eba09
FranciscoDiego64 <112688191+FranciscoDiego64@users.noreply.github.com>
2023-05-28 12:36:50 +0200

    installed Material-UI and styled login and registry pages

--- src/components/Register.js
+++ src/components/Register.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { createUserWithEmailAndPassword } from "firebase/auth"; 
 import { auth } from '../firebase'; 
 import { useNavigate } from 'react-router-dom';
+import { TextField, Button, Typography, Container, Box } from '@mui/material';
 
 const Register = () => {
   const [email, setEmail] = useState('');
@@ -13,7 +14,7 @@ const Register = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await createUserWithEmailAndPassword(auth, email, password); // modified this line
+      await createUserWithEmailAndPassword(auth, email, password);
       navigate('/profile'); 
     } catch (error) {
       setErrorMessage(error.message);
@@ -21,18 +22,58 @@ const Register = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      {errorMessage && <p>{errorMessage}</p>}
-      <label>
-        Email:
-        <input type="email" value={email} onChange={e => setEmail(e.target.value)} required />
-      </label>
-      <label>
-        Password:
-        <input type="password" value={password} onChange={e => setPassword(e.target.value)} required />
-      </label>
-      <input type="submit" value="Register" />
-    </form>
+    <Container maxWidth="sm">
+      <Box
+        component="form"
+        onSubmit={handleSubmit}
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          mt: 8,
+        }}
+      >
+        <Typography variant="h4" gutterBottom>
+          Register
+        </Typography>
+        {errorMessage && <p>{errorMessage}</p>}
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          id="email"
+          label="Email Address"
+          name="email"
+          autoComplete="email"
+          autoFocus
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+        />
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          name="password"
+          label="Password"
+          type="password"
+          id="password"
+          autoComplete="current-password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+        />
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          color="primary"
+          sx={{ mt: 3 }}
+        >
+          Register
+        </Button>
+      </Box>
+    </Container>
   );
 };
 
--- src/components/login.js
+++ src/components/login.js
@@ -1,49 +1,87 @@
 import React, { useState } from 'react';
-import { useNavigate, Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../firebase';
-
+import { TextField, Button, Typography, Container, Box } from '@mui/material';
 
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
-
   const navigate = useNavigate();
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
     try {
       await signInWithEmailAndPassword(auth, email, password);
-      setErrorMessage('');
-      navigate('/profile'); // Redirect to profile page after login
+      navigate('/profile'); // Redirect to profile page after successful login
     } catch (error) {
-      setErrorMessage(error.message);
+      console.error(error);
     }
   };
 
-  const handleRegisterClick = () => {
-    navigate('/register');
-  };
-
   return (
-    <form onSubmit={handleSubmit}>
-      {errorMessage && <p>{errorMessage}</p>}
-      <label>
-        Email:
-        <input type="email" value={email} onChange={e => setEmail(e.target.value)} required />
-      </label>
-      <label>
-        Password:
-        <input type="password" value={password} onChange={e => setPassword(e.target.value)} required />
-      </label>
-      <input type="submit" value="Log in" />
-      <p>Don't have an account? <Link to="/register">Register here bro</Link>.</p>
-    </form>
+    <Container maxWidth="sm">
+      <Box
+        component="form"
+        onSubmit={handleSubmit}
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          mt: 8,
+        }}
+      >
+        <Typography variant="h4" gutterBottom>
+          Login
+        </Typography>
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          id="email"
+          label="Email Address"
+          name="email"
+          autoComplete="email"
+          autoFocus
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <TextField
+          variant="outlined"
+          margin="normal"
+          required
+          fullWidth
+          name="password"
+          label="Password"
+          type="password"
+          id="password"
+          autoComplete="current-password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          color="primary"
+          sx={{ mt: 3 }}
+        >
+          Sign In
+        </Button>
+        <Button
+          fullWidth
+          variant="outlined"
+          color="secondary"
+          onClick={() => navigate('/register')}
+        >
+          New user? Register here
+        </Button>
+      </Box>
+    </Container>
   );
 };
 
 export default Login;
-
-
 
